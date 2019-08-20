2a17ad9eb24a8bd7422f10d482859c24bd8b21e8
Aleksejs Muzičenko <aleksejs@mendo.lv>
2019-08-20 19:17:43 +0300

    Mongoose added

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ const express = require('express');
 const path = require('path');
 const expressEdge = require('express-edge');
 
+const mongoose =require('mongoose');
+
 const app = new express();
+
+mongoose.connect('mongodb://database-service:27017/node-blog-app', {useNewUrlParser: true } )
 
 app.use(express.static('public'));
 app.use(expressEdge);
