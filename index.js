ce2e43290b9e3805c5d27920903885af37492f8b
Aleksejs Muzičenko <aleksejs@mendo.lv>
2019-08-20 15:03:03 +0300

    More links

--- index.js
+++ index.js
@@ -9,6 +9,18 @@ app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'pages/index.html'))
 })
 
+app.get('/about', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'pages/about.html'))
+})
+
+app.get('/contact', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'pages/contact.html'))
+})
+
+app.get('/post', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'pages/post.html'))
+})
+
 app.listen(5555, () => {
   console.log('App listening on port 5555');
 });
