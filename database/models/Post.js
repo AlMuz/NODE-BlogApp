1c3659456216185966d41a4dcedec538fcaf51eb
Aleksejs Muzičenko <aleksejs@mendo.lv>
2019-08-21 13:16:48 +0300

    Added username and createdAt date to the post and displayed on pages

--- database/models/Post.js
+++ database/models/Post.js
@@ -4,7 +4,9 @@ const mongoose = require('mongoose');
 const PostSchema = new mongoose.Schema({
   title: {type: String, require: true},
   description: {type: String, require: true},
-  content: {type: String, require: true}
+  content: {type: String, require: true},
+  username: {type: String, require: true},
+  createdAt: {type: Date, default: Date.now}
 })
 
 const Post = mongoose.model('Post', PostSchema)
